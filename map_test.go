747624749e42fd0ae024574e68ff56e06f53343d
eggnocent <egiwiratamaaa@gmail.com>
2024-08-13 10:35:03 +0700

    add testing encode mao

--- map_test.go
+++ map_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestMap(t *testing.T) {
+func TestMapDecode(t *testing.T) {
 	jsonString := `{"id": "P01","name": "Lenovo v15", "price": 20000}`
 	jsonBytes := []byte(jsonString)
 
@@ -17,4 +17,14 @@ func TestMap(t *testing.T) {
 	fmt.Println(result["id"])
 	fmt.Println(result["name"])
 	fmt.Println(result["price"])
+}
+
+func TestMapEncode(t *testing.T) {
+	product := map[string]interface{}{
+		"id":    "P01",
+		"name":  "Lenovo1",
+		"price": 9000000,
+	}
+	bytes, _ := json.Marshal(product)
+	fmt.Println(string(bytes))
 }
